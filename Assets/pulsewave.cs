2d87dae12b036c16783628329fbee36176012eec
Kari Barry <kevin.a.barry@stonybrook.edu>
2019-01-07 17:16:09 -0500

    Default scene created, cube made and attached to camera which now has WASD controls in lieu of VR. Clicking and holding emulates the cube growth exponentially through time which will be replaced with the trigger of the Vive Controller. Cube growth will be limited by the overall sample size as soon as some test data is available

---
+++ Assets/pulsewave.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pulsewave : MonoBehaviour
+{
+    Vector3 value;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            //OnMouseClick and hold, box increaces in size, colliding with points and generating events
+            //Eventually will also be mapped to vive controller trigger
+            float t = Time.deltaTime;
+            value = transform.localScale;
+            value.x += t;
+            value.y += t;
+            value.z += t;
+
+            transform.localScale = value;
+        }
+    }
+}
